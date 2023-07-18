c5f93d306d86c2d81701dbaa4fc2f609f3e507ae
Nachanon777 <137865597+Nachanon777@users.noreply.github.com>
2023-07-18 15:47:17 +0700

    week05

---
+++ database/migrations/2023_07_18_153135_create_employee_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            
+            $table->string('name')->nullable();
+            $table->date('birthday')->nullable();
+            $table->float('salary')->nullable();
+            $table->text('address')->nullable();
+            $table->integer('phone number')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
